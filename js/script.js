6ae9533bde438b6cd65f20309959f94e5e547183
pierobramanti <piero.bramanti94@gmail.com>
2024-07-25 17:49:19 +0200

    [UPD] Use for loop to push email from API response

--- js/script.js
+++ js/script.js
@@ -7,9 +7,13 @@ createApp({
         }
     },
     methods: {
-        getEmails() {
+        generateEmails() {
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
-
+                console.log(resp)
+                for(let i = 0; i<10; i++) {
+                    this.emails.push(resp.data.response)
+                }
+                console.log(resp.data.response)
             })
         }
     }
