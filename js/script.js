506d2a99cd6632674c19223f360cbc77d35730ab
pierobramanti <piero.bramanti94@gmail.com>
2024-07-25 18:10:47 +0200

    [UPD] added list item with v-for, email are now printed in dom

--- js/script.js
+++ js/script.js
@@ -9,11 +9,11 @@ createApp({
     methods: {
         generateEmails() {
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
-                console.log(resp)
+                
                 for(let i = 0; i<10; i++) {
                     this.emails.push(resp.data.response)
                 }
-                console.log(resp.data.response)
+                
             })
         }
     }
