40d991a5c9be43c1f812f40a791ab8a9c578eda4
pierobramanti <piero.bramanti94@gmail.com>
2024-07-25 17:29:23 +0200

    [UPD] Add Axios GET request for random email

--- js/script.js
+++ js/script.js
@@ -3,10 +3,14 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-
+            emails: []
         }
     },
     methods: {
+        getEmails() {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
 
+            })
+        }
     }
 }).mount("#app")
