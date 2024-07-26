fa2a97b2b612145392ef21566bebd86c4efa1d02
pierobramanti <piero.bramanti94@gmail.com>
2024-07-26 15:27:40 +0200

    [UPD] fixed with class correction

--- js/script.js
+++ js/script.js
@@ -6,15 +6,19 @@ createApp({
             emails: []
         }
     },
+
+    created() {
+        this.generateEmails();
+    },
     methods: {
         generateEmails() {
-            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
-                
-                for(let i = 0; i<10; i++) {
+           
+            for(let i = 0; i< 10; i++) {            
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
                     this.emails.push(resp.data.response)
-                }
+                });
                 
-            })
+            }
         }
     }
 }).mount("#app")
